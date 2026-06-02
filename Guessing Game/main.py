14e4448946078f1caf9dcce8d0e2dbb18c15457c
Mohd Bilal <billusaifi063@gmail.com>
2026-06-03 01:07:58 +0530

    Update main.py

--- Guessing Game/main.py
+++ Guessing Game/main.py
@@ -4,14 +4,14 @@ from file_logic import *
 
 print("Welcome to Our Game\n")
 
-high_score=6
+high_score=file_load()
 
 while True:
     Computer_number = random.randint(1, 25)
 
     game_won, high_score = attempt_logic(Computer_number, high_score)
 
-    file_load()
+    
     file_handling(high_score)
 
     if game_won:
